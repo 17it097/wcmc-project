00db707eadbc147c20ec5b1bbba6223ddfb41796
17it097 <17it097@charusat.edu.in>
2020-04-12 19:37:44 +0530

    wcmc project

---
+++ app/src/main/java/com/example/a17it097/MenuItemData.java
@@ -0,0 +1,11 @@
+package com.example.a17it097;
+
+public class MenuItemData {
+
+    public String mTitle="helo";
+
+    public MenuItemData(String mTitle)
+    {
+        this.mTitle = mTitle;
+    }
+}
